e7c3b7f3045570a24773d6781ee2e30ea4f902e8
raviramamoorthy <ravibharathii@gmail.com>
2016-11-24 19:58:28 -0700

    Added test files

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,10 @@
+/**
+ * Created by raviramamoorthy on 11/24/16.
+ */
+
+
+
+require('babel-register')();
+require.extensions['.css'] = function () {
+    
+};
---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by raviramamoorthy on 11/24/16.
+ */
+import {expect} from 'chai';
+
+describe('Our first test',() =>{
+    "use strict";
+    it('should pass', () =>{
+        expect(true).to.equal(true);
+    })
+
+});
